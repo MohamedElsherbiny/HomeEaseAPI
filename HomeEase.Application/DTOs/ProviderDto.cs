5d6d845f474655d2ce97da0cda478f2d5917dc09
Mohamed Elsherbiny <mohamed.elsherbiny@fizapp.io>
2025-05-23 21:52:40 +0300

    Add IsActive to ProviderDto

--- HomeEase.Application/DTOs/ProviderDto.cs
+++ HomeEase.Application/DTOs/ProviderDto.cs
@@ -9,6 +9,7 @@ namespace HomeEase.Application.DTOs
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
+        public bool IsActive { get; set; } = true;
         public string BusinessName { get; set; }
         public string Description { get; set; }
         public string ProfileImageUrl { get; set; }
