be45abfdc1dea79af6702d732dfe56673c74a52c
Mohamed Elsherbiny <mohamed.elsherbiny@gotocme.com>
2025-07-13 22:44:58 +0300

    Fix bookings endpoint

---
+++ HomeEase.Infrastructure/Migrations/20250713193859_UpdateBookings.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeEase.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+               name: "AppointmentDateTime",
+               table: "Bookings",
+               type: "datetime2",
+               nullable: false,
+               defaultValue: DateTime.Now); // or use a more reasonable default
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+               name: "AppointmentDateTime",
+               table: "Bookings");
+        }
+    }
+}
