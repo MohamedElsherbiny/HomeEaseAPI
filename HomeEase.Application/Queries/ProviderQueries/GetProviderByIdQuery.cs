1cf6c4a4ea2a16021b5e7ba8cd89a2d927c0b816
Mohamed Elsherbiny <mohamed.elsherbiny@gotocme.com>
2025-08-06 19:41:00 +0300

    don't return providers services equal to 0

--- HomeEase.Application/Queries/ProviderQueries/GetProviderByIdQuery.cs
+++ HomeEase.Application/Queries/ProviderQueries/GetProviderByIdQuery.cs
@@ -1,8 +1,8 @@
 ﻿using AutoMapper;
 using HomeEase.Application.DTOs.Provider;
-using HomeEase.Application.Queries.ProviderQueries;
-using HomeEase.Domain.Repositories;
+using HomeEase.Application.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomeEase.Application.Queries.ProviderQueries;
 
@@ -11,11 +11,30 @@ public class GetProviderByIdQuery : IRequest<ProviderDto>
     public Guid ProviderId { get; set; }
 }
 
-public class GetProviderByIdQueryHandler(IProviderRepository _providerRepository, IMapper _mapper) : IRequestHandler<GetProviderByIdQuery, ProviderDto>
+public class GetProviderByIdQueryHandler(IAppDbContext _dbContext, IMapper _mapper) : IRequestHandler<GetProviderByIdQuery, ProviderDto>
 {
     public async Task<ProviderDto> Handle(GetProviderByIdQuery request, CancellationToken cancellationToken)
     {
-        var provider = await _providerRepository.GetByIdWithDetailsAsync(request.ProviderId);
+        var provider = await _dbContext.Providers
+            .Include(p => p.Address)
+            .Include(p => p.User)
+            .Include(p => p.Schedule)
+                .ThenInclude(s => s.RegularHours)
+            .Include(p => p.Schedule)
+                .ThenInclude(s => s.SpecialDates)
+            .Include(p => p.Schedule)
+                .ThenInclude(s => s.AvailableSlots)
+            .Include(p => p.Services).ThenInclude(s => s.BasePlatformService)
+            .Include(p => p.Images)
+            .FirstOrDefaultAsync(p => p.Id == request.ProviderId);
+
+        if (provider is null)
+        {
+            return null;
+        }
+
+        provider.Services = [.. provider.Services.Where(s => s.Price > 0 || s.HomePrice > 0)];
+
 
         return _mapper.Map<ProviderDto>(provider);
     }
